6573dd1921b3cc44d452cc49b70888b12934cdd4
ahmed achour <66746240+17med@users.noreply.github.com>
2023-06-26 19:02:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 3000;
 app.get("/",(req,res)=>{
     res.send("<center><h1>appGenuis work</h1></center>")
 })
-app.listen(port,()=>{console.log("server start")})
+app.listen(port,()=>{console.log("server start ${PORT}")})
